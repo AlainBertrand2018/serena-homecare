ee31d8cdbf1e80ad4fa5e3bb78415347a6fb7179
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-07-30 20:40:51 +0000

    TypeError: (0 , __TURBOPACK__imported__module__$5b$project$5d2f$node_mod

--- src/components/care-log-summary.tsx
+++ src/components/care-log-summary.tsx
@@ -1,7 +1,8 @@
 
 'use client';
 
-import { useActionState, useFormStatus } from 'react';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { getSummary } from '@/lib/actions';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
