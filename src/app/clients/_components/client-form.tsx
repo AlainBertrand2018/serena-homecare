a2693a2995d53ca924fe570becea2e986480175a
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-07-30 20:30:20 +0000

    New client is still not listed

--- src/app/clients/_components/client-form.tsx
+++ src/app/clients/_components/client-form.tsx
@@ -15,7 +15,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { Client } from "@/lib/data";
+import type { Client } from "@/lib/data";
 
 interface ClientFormProps {
     client?: Client;
