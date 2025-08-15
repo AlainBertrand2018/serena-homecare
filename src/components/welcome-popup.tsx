c2cd3e35a379b7cd541b07cea811f56ba85a4db9
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-08-15 09:25:59 +0000

    go

--- src/components/welcome-popup.tsx
+++ src/components/welcome-popup.tsx
@@ -48,17 +48,18 @@ export function WelcomePopup() {
               data-ai-hint="logo"
             />
           </div>
-          <AlertDialogTitle>La Gestion de Votre Service de Soins à la Personne en Toute Intelligence</AlertDialogTitle>
+          <AlertDialogTitle>La Gestion de Votre Service de Soins à Domicile en Toute Sérénité</AlertDialogTitle>
           <AlertDialogDescription asChild>
             <div className='space-y-3'>
               <p>
-                Optimisez vos opérations, assurez la qualité et la rentabilité de votre service avec un suivi intelligent en temps réel.
+                Concentrez vous sur votre coeur de métier, le service à la personne. Optimisez vos opérations, assurez la qualité et la rentabilité de votre service avec un suivi intelligent en temps réel.
               </p>
               <ul className="list-disc list-inside space-y-1 text-left">
-                  <li>Tableau de bord pour une vue d'ensemble : Comptabilité, Calendrier, Clients, Personnels</li>
-                  <li>Gestion et recrutement des aides-soignants</li>
+                  <li>Tableau de bord pour une vue d'ensemble : Comptabilité, Calendriers, Clients, Personnels</li>
+                  <li>Recrutement et gestion des aides-soignants</li>
                   <li>Intégration clients simplifiée</li>
                   <li>Prise de Rendez-vous et gestion des visites à domicile</li>
+                  <li>Service de newsletter periodique intégré</li>
               </ul>
               <p className="font-bold pt-2">L’humain d’abord, le reste en un clic.</p>
             </div>
